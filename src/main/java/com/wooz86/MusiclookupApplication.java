e28e850469eaf636edd4a600b772815067d90fed
Joel Thoresson <joel@netconsult.se>
2016-08-27 00:14:37 +0200

    Initial commit

---
+++ src/main/java/com/wooz86/MusiclookupApplication.java
@@ -0,0 +1,12 @@
+package com.wooz86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiclookupApplication.class, args);
+	}
+}
