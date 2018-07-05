584043558d3f68239c024e6fe310845cc91176ae
Joel Thoresson <joel@netconsult.se>
2018-07-05 22:26:35 +0200

    Playing with concourse

--- ci/gradle-build.sh
+++ ci/gradle-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export GRADLE_USER_HOME="./source-code/.gradle"
 
+export GRADLE_USER_HOME="./source-code/.gradle"
+mkdir $GRADLE_USER_HOME
 chmod +x $GRADLE_USER_HOME
 
 ./source-code/gradlew build
