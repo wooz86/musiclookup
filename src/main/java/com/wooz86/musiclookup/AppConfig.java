bfbb5e8473b9c633d30d47cea5ab0ba34c4c621c
Joel Thoresson <joel@netconsult.se>
2018-07-14 18:34:28 +0200

    Merge Java config classes to a single file

--- src/main/java/com/wooz86/musiclookup/AppConfig.java
+++ src/main/java/com/wooz86/musiclookup/AppConfig.java
@@ -6,12 +6,21 @@ import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableCaching
-public class CachingConfig {
+public class AppConfig {
+
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newFixedThreadPool(10);
+    }
 
     @Bean
     public CacheManager cacheManager() {
@@ -20,4 +29,10 @@ public class CachingConfig {
 
         return cacheManager;
     }
+
+    @Bean
+    public RestOperations restTemplate() {
+        return new RestTemplate();
+    }
+
 }
